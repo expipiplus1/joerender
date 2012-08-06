35c37f5ebea3bea1baa6a90e5f24c4dcd7be9d17
Joe Hermaszewski <expipiplus1@gmail.com>
2012-08-07 00:49:11 +0100

    [+] Using JoeLang

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <cassert>
 #include <iostream>
 
 #include <GL/glfw3.h>
+
+#include <joelang/context.hpp>
+#include <joelang/effect.hpp>
 
 bool running = true;
 
@@ -27,7 +31,10 @@ int main()
     }
 
     // Create a window
-    GLFWwindow window = glfwCreateWindow( 640, 480, GLFW_WINDOWED, "JoeRender", NULL );
+    GLFWwindow window = glfwCreateWindow( 640, 480,
+                                          GLFW_WINDOWED,
+                                          "JoeRender",
+                                          NULL );
     if (!window)
     {
         std::cerr << "Failed to open GLFW window\n";
@@ -35,9 +42,15 @@ int main()
         return -1;
     }
 
-    std::cout << "GL_RENDERER = " << reinterpret_cast<const char*>(glGetString(GL_RENDERER)) << std::endl;
-    std::cout << "GL_VERSION  = " << reinterpret_cast<const char*>(glGetString(GL_VERSION)) << std::endl;
-    std::cout << "GL_VENDOR   = " << reinterpret_cast<const char*>(glGetString(GL_VENDOR)) << std::endl;
+    std::cout << "GL_RENDERER = " <<
+                 reinterpret_cast<const char*>(glGetString(GL_RENDERER)) <<
+                 std::endl;
+    std::cout << "GL_VERSION  = " <<
+                 reinterpret_cast<const char*>(glGetString(GL_VERSION)) <<
+                 std::endl;
+    std::cout << "GL_VENDOR   = " <<
+                 reinterpret_cast<const char*>(glGetString(GL_VENDOR)) <<
+                 std::endl;
 
     glfwSetInputMode( window, GLFW_KEY_REPEAT, GL_TRUE );
     glfwSwapInterval( 1 );
@@ -46,9 +59,25 @@ int main()
     glfwSetWindowCloseCallback( OnWindowClosed );
     glfwSetKeyCallback( OnKeyInput );
 
+    //
+    // Initialize JoeLang
+    //
+    JoeLang::Context context;
+    context.RegisterOpenGLStates();
+    context.RegisterOpenGLActions();
+    JoeLang::Effect* clear_blue =
+                          context.CreateEffectFromFile( "data/clear_blue.jfx" );
+    assert( clear_blue && "Couldn't get effect" );
+
+    const JoeLang::Technique* t = clear_blue->GetNamedTechnique( "clear_blue" );
+    assert( t );
+
     // Main loop
     while( running )
     {
+        for( const JoeLang::Pass& pass : t->GetPasses() )
+            pass.SetState();
+
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
